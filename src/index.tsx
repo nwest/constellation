583a9d04d79600d49ac4f7208499655cb5d14043
Nate West <nwestr@gmail.com>
2019-05-09 15:25:16 -0400

    Move the reducer

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Action, createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-const reducer = (state: any = { following: [] }, action: Action) => {
-    return state;
-};
+import * as serviceWorker from './serviceWorker'
+    ;
+import { createStore } from 'redux';
+import { reducer } from './reducers/reducer';
+import {Provider} from "react-redux";
 
 const store = createStore(reducer);
 
---
+++ src/reducers/reducer.ts
@@ -0,0 +1,5 @@
+import { Action } from "redux";
+
+export const reducer = (state: any = { repos: [] }, action: Action) => {
+    return state;
+};
