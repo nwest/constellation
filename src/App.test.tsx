4f1cc6b05ae85f174ba7e5d2b3a9af620c531310
Nate West <nwestr@gmail.com>
2019-05-10 14:44:12 -0400

    Fix test title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('says Hello World!', () => {
+it('says See your Starred Repos:', () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   expect(div.innerHTML).toMatch("See your Starred Repos:");
